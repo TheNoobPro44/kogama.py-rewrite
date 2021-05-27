a80a83feffb347113e39c247310ac21e9cbdc2a1
TheNoobPro44 <67636254+TheNoobPro44@users.noreply.github.com>
2021-05-27 13:57:22 -0300

    Add files via upload

---
+++ kogama/__init__.py
@@ -0,0 +1,2 @@
+from kogama.Kogama import KoGaMa
+from .Exceptions import DisallowedURlInput, NotAValidServer, InvalidInformation, FailedLogin, FeedError, TooMuchRequests, ReasonNotFound, TemplateNotFound
