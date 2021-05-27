62c0f2b4f8fde8d81b0184f9453092a6eed281e3
TheNoobPro44 <67636254+TheNoobPro44@users.noreply.github.com>
2021-05-27 14:33:55 -0300

    Delete kogama directory

--- kogama/__init__.py
+++
@@ -1,2 +0,0 @@
-from kogama.kogama import KoGaMa
-from exceptions import DisallowedURlInput, NotAValidServer, InvalidInformation, FailedLogin, FeedError, TooMuchRequests, ReasonNotFound, TemplateNotFound
