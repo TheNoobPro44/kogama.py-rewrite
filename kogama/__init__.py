7454d0900f7441912cb6799725d25245dfab8292
TheNoobPro44 <67636254+TheNoobPro44@users.noreply.github.com>
2021-05-27 14:03:08 -0300

    Update __init__.py

--- kogama/__init__.py
+++ kogama/__init__.py
@@ -1,2 +1,2 @@
-from kogama.Kogama import KoGaMa
+from kogama.kogama import KoGaMa
 from .Exceptions import DisallowedURlInput, NotAValidServer, InvalidInformation, FailedLogin, FeedError, TooMuchRequests, ReasonNotFound, TemplateNotFound
