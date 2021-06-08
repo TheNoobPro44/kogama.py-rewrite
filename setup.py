7919dc4d54441d8af0f3db28e4797d8906557d94
TheNoobPro44 <67636254+TheNoobPro44@users.noreply.github.com>
2021-06-08 14:02:30 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,14 +9,16 @@ setuptools.setup(
     version="0.4",
     author="TheNoobPro44",
     author_email="TheNewbiePro44@gmail.com",
-    description="KoGaMa.py-Rewrite is a API-wrapper for KoGaMa.",
+    description="KoGaMa.py-Rewrite is an API-wrapper for KoGaMa.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/TheNoobPro44/KoGaMa.py-Rewrite/",
     project_urls={
         "Bug Tracker": "https://github.com/TheNoobPro44/KoGaMa.py-Rewrite/issues",
-    },
+        "GitHub": "https://github.com/KILR007/roblox.py",
+        "Examples": "https://github.com/TheNoobPro44/kogama.py-rewrite/blob/main/example/example.py"},
     classifiers=[
+        "Development Status :: 4 - Beta"
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
