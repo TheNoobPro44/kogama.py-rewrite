78f591057224697f79077360272f28261470c717
TheNoobPro44 <67636254+TheNoobPro44@users.noreply.github.com>
2021-06-09 15:06:20 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/TheNoobPro44/KoGaMa.py-Rewrite/issues",
         "Examples": "https://github.com/TheNoobPro44/kogama.py-rewrite/blob/main/example/example.py"},
     classifiers=[
-        "Development Status :: 4 - Beta"
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
