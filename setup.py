b057958c9674c98ba75f3e8c28f93a1147a6e876
TheNoobPro44 <67636254+TheNoobPro44@users.noreply.github.com>
2021-06-09 16:04:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="KoGaMa.py-Rewrite",
     packages = ['Kogama'],
-    version="0.4.5",
+    version="0.4.6",
     author="TheNoobPro44",
     author_email="TheNewbiePro44@gmail.com",
     description="KoGaMa.py-Rewrite is an API-wrapper for KoGaMa.",
