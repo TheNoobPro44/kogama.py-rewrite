db6cf3e1c591a45921f2c3c993749cc2fffd3188
TheNoobPro44 <67636254+TheNoobPro44@users.noreply.github.com>
2021-06-10 13:39:29 -0300

    Update kogama.py

--- Kogama/kogama.py
+++ Kogama/kogama.py
@@ -3,49 +3,44 @@
  * Scripted by: TheNoobPro44 (With the help of: MD & Tokeeto!)
  * Originaly Made By: Ars3ne.
 '''
-import os
 import requests
-import time
 import json
-from requests.sessions import Session, session
-from .Exceptions import DisallowedURlInput, NotAValidServer, InvalidInformation, FailedLogin, FeedError, TooMuchRequests, ReasonNotFound, TemplateNotFound, FieldIsRequired
+import requests
+from .Exceptions import DisallowedURlInput, NotAValidServer, FailedLogin, TooMuchRequests, ReasonNotFound, TemplateNotFound
 
 class KoGaMa:
     def __init__(self, server):
-        if server.lower() not in ('www', 'br', 'friends'):
-            raise NotAValidServer('Not a valid server!')
+      if server.lower() not in ('www', 'br', 'friends'):
+          raise NotAValidServer('Not a valid server!')
         
-        self.user_id = None
-        self.url = {'br': 'https://kogama.com.br',
-                    'www': 'https://www.kogama.com',
-                    'friends': 'https://friends.kogama.com',
-                   }[server.lower()]
+      self.user_id = None
+      self.url = {'br': 'https://kogama.com.br',
+                  'www': 'https://www.kogama.com',
+                  'friends': 'https://friends.kogama.com',
+                  }[server.lower()]
 
-        self.session = requests.Session()
+      self.session = requests.Session()
 
     def Login(self, username, password):
         """
-        Makes login in a KoGaMa account, given the Username & Password.
+        - Makes login in a KoGaMa account, given the Username & Password.
 
         Returns True, If the user has logged in.
         Returns False, If the user could not login.
-        
-        **Parameters**
+
+        Parameters:
         ----------
-        username : str
-            User's account name.
-        password : str
-            User's account password.
+          username : str
+             User's account name.
+          password : str
+             User's account password.
         """
-        if username or password == " ":
-            raise FieldIsRequired("Hey. This field is required, please input your username and password!")
         data = {"username": username, "password": password}
         response = self.session.post(f"{self.url}/auth/login/", json=data)
         if 'error' not in response:
           return True
         elif 'error' in response:
           return False
-        
         if response.status_code != 200:
           raise FailedLogin("Please check If your Password / Username is correct and try again..")
         else:
@@ -53,13 +48,13 @@ class KoGaMa:
 
     def Logout(self):
       """
-      Logout a user from his KoGaMa account.
+      - Logout a user from his KoGaMa account.
 
       Returns True, If the user has logged out.
       
-      **Parameters**
+      Parameters:
       ----------
-      No Parameters..
+        No Parameters..
       """
       self.session.get(f"{self.url}/auth/logout/")
       self.session.cookies.clear()
@@ -67,20 +62,18 @@ class KoGaMa:
 
     def PostFeed(self, userID, message):
         """
-        Post a message in user's Feed.
+        - Post a message in user's Feed.\n
 
-        Returns True, If the message has been sent.
+        Returns True, If the message has been sent.\n
         Returns False, If message fails to send.
         
-        **Parameters**
+        Parameters:
         ----------
-        userID : int
-            ID of the user.
-        message : str
-            Message that will be posted.
+          userID : int / str
+             ID of the user.
+          message : str
+             Message that will be posted.
         """
-        if message or userID == " ":
-            raise FieldIsRequired("Hey. This field is required, please input your Message!")
         url2 = self.url
         uid = userID
         data = {"status_message": message,"profile_id": userID,"wait": True}
@@ -88,8 +81,6 @@ class KoGaMa:
         response2 = response.text
         if response.status_code != 200:
           return False
-        if response.status_code != 200:
-          raise FeedError("An error ocurred while trying to post a message in your Feed!")
         if response.status_code == 200:
           return True
         if 'Disallowed' in response2:
@@ -97,19 +88,18 @@ class KoGaMa:
 
     def ReportUser(self, userID, reason):
       """
-      Reports a users..
+      - Reports a users..\n
 
-      Returns True, If the user has been reported.
+      Returns True, If the user has been reported.\n
       Returns False, If fails to report a user.
-      **Parameters**
-        ----------
-        userID : int
+
+      Parameters:
+      ----------
+        userID : int / str
             The ID of the user being reported.
         reason : str
             Reason of report.
       """
-      if userID or reason == " ":
-        raise FieldIsRequired("Hey. This field is required, please input the user ID or the Report Reason!")
       reports={"sharing_personal_information":1, "sharing_password": 2, "use_of_profanity": 3, "sexual_content_or_behaviour": 4, "violent_content": 5, "chain_messages": 6, "pretend_to_be_admin": 7, "personal_threats": 8, "cheats & hacking": 9, "other": 10, "using_cheat_tool": 11}
       url2 = self.url
       rl = reason.lower()
@@ -130,15 +120,13 @@ class KoGaMa:
     
     def GetPostComments(self, postID):
         """
-        Get comments from a post and return it.
+        - Get comments from a post and return it.
         
-        **Parameters**
+        Parameters:
         ----------
-        postID : int
-            ID of the Post.
+          postID : int / str
+             ID of the Post.
         """
-        if postID == " ":
-            raise FieldIsRequired("Hey. This field is required, please input your Post ID!")
         url2 = self.url
         response = self.session.get(f'{url2}/api/feed/{postID}/comment/')
         response2 = json.loads(response.text)
@@ -149,20 +137,18 @@ class KoGaMa:
 
     def PostGameComment(self, GameID, message):
       """
-      Post a comment in a Game.
+      - Post a comment in a Game.\n
 
-      Returns True, If the comment has been posted.
+      Returns True, If the comment has been posted.\n
       Returns False, If fails to post a comment.
       
-      **Parameters**
-        ----------
-        GameID : int
+      Parameters:
+      ----------
+        GameID : int / str
             ID of the Game.
         message : str
             Message that will be posted.
       """
-      if GameID or message == " ":
-          raise FieldIsRequired("Hey. This field is required, please input your Message or Game ID!")
       url2 = self.url
       data = {"comment":message}
       response = self.session.post(f"{url2}/game/{GameID}/comment/", json=data)
@@ -176,20 +162,18 @@ class KoGaMa:
 
     def PostModelComment(self, ModelID, message):
       """
-      Post a comment in a Model.
+      - Post a comment in a Model.\n
 
-      Returns True, If the comment has been posted.
+      Returns True, If the comment has been posted.\n
       Returns False, If fails to post a comment.
       
-      **Parameters**
-        ----------
-        ModelID : int
+      Parameters:
+      ----------
+        ModelID : int / str
             ID of the Model.
         message : str
             Message that will be posted.
       """
-      if message or ModelID == " ":
-            raise FieldIsRequired("Hey. This field is required, please input your Message or Model ID!")
       url2 = self.url
       data = {"comment":message}
       response = self.session.post(f"{url2}/model/market/i-{ModelID}/comment/", json=data)
@@ -203,13 +187,19 @@ class KoGaMa:
     
     def PostAvatarComment(self, AvatarID, message):
       """
-      Post a comment in a Avatar.
+      - Post a comment in a Avatar.\n
 
-      Returns True, If the comment has been posted.
+      Returns True, If the comment has been posted.\n
       Returns False, If fails to post a comment.
+
+      Parameters:
+      ----------
+        AvatarID : int / str
+            ID of the Avatar.
+        message : str
+            Message that will be posted.
+
       """
-      if message or AvatarID == " ":
-          raise FieldIsRequired("Hey. This field is required, please input your message or Avatar ID!")
       url2 = self.url
       data = {"comment":message}
       response = self.session.post(f"{url2}/model/market/a-{AvatarID}/comment/")
@@ -223,20 +213,18 @@ class KoGaMa:
 
     def PostNewsComment(self, newsID, message):
         """
-        Post a comment in a News Page.
+        - Post a comment in a News Page.\n
         
-        Returns True, If the comment has been posted.
+        Returns True, If the comment has been posted.\n
         Returns False, If fails to post a comment.
         
-        **Parameters**
+        Parameters:
         ----------
-        newsID : int
+        newsID : int / str
             ID of the News.
         message : str
             Message that will be posted.
         """
-        if newsID or message == " ":
-            raise FieldIsRequired("Hey. This field is required, please input your message or News ID!")
         url2 = self.url
         data = {"comment":message}
         response = self.session.post(f"{url2}/api/news/{newsID}/comment/", json=data)
@@ -251,13 +239,13 @@ class KoGaMa:
     
     def CreateGame(self, Name, Desc, Template):
       """
-      Creates a game.
+      - Creates a game.\n
 
-      Returns True, If the game has been created.
+      Returns True, If the game has been created.\n
       Returns False, If fails to create a game.
       
-      **Parameters**
-        ----------
+      Parameters:
+      ----------
         Name : str
             Name of your Game.
         Desc : str
@@ -265,12 +253,6 @@ class KoGaMa:
         Template : str
             Template of your Game.
       """
-      if Name == " ":
-          raise FieldIsRequired("Hey. This field is required, please input your Game Name!")
-      if Desc == " ":
-          raise FieldIsRequired("Hey. This field is required, please input your Game Description!")
-      if Template == " ":
-          raise FieldIsRequired("Hey. This field is required, please input your Game Template!")
       url2 = self.url
       tmplt = Template.lower()
       tmplt2 = tmplt.replace(" ", "_")
@@ -290,20 +272,18 @@ class KoGaMa:
 
     def InviteMemberToGame(self, GameID, UserID):
       """
-      Invites a member to a Project or Game.
+      - Invites a member to a Project or Game.\n
 
-      Returns True, If the user has been invited.
+      Returns True, If the user has been invited.\n
       Returns False, If fails to invite a user.
       
-      **Parameters**
-        ----------
-        GameID : int
+      Parameters:
+      ----------
+        GameID : int / str
             ID of the Game.
-        UserID : int
+        UserID : int / str
             ID of the User.
       """
-      if GameID or UserID == " ":
-          raise FieldIsRequired("Hey. This field is required, please input your Game ID or User ID!")
       url2 = self.url
       data = {"game_id":GameID,"member_user_id":UserID}
       response = self.session.post(f"{url2}/game/{GameID}/member/", json=data)
@@ -316,18 +296,16 @@ class KoGaMa:
     # Friend Category..
     def SendFriendRequest(self, friendID):
         """
-        Sends a friend request to a user.
+        - Sends a friend request to a user.\n
 
-        Returns True, If a friend request has been sent.
+        Returns True, If a friend request has been sent.\n
         Returns False, If fails to send a friend request.
         
-        **Parameters**
+        Parameters:
         ----------
-        UserID : int
-            ID of the User.
+          UserID : int / str
+             ID of the User.
         """
-        if friendID == " ":
-            raise FieldIsRequired("Hey. This field is required, please input your friend's Profile ID!")
         url2 = self.url
         uid = self.user_id
         data = {"friend_profile_id":friendID,"profile_id":uid,"user_id":friendID}
@@ -339,18 +317,16 @@ class KoGaMa:
             return False
     def CancelFriendRequest(self, friendID):
       """
-      Cancels a friend request.
+      - Cancels a friend request.\n
 
-      Returns True, If a friend request has been cancelled.
+      Returns True, If a friend request has been cancelled.\n
       Returns False, If fails to cancel a friend request.
       
-      **Parameters**
-        ----------
-        UserID : int
+      Parameters:
+      ----------
+        UserID : int / str
             ID of the User.
       """
-      if friendID == " ":
-        raise FieldIsRequired("Hey. This field is required, please input your friend's Profile ID!")
       url2 = self.url
       uid = self.user_id
       response = self.session.delete(f"{url2}/user/{uid}/friend/{friendID}/")
@@ -364,18 +340,16 @@ class KoGaMa:
                                  
     def PurchaseModel(self, modelID):
        """
-       Purchases a Model from Shop.
+       - Purchases a Model from Shop.\n
        
-       Returns True, If model has been bought.
+       Returns True, If model has been bought.\n
        Returns False, If fails to buy model.
        
-       **Parameters**
-        ----------
-        modelID : int
+       Parameters:
+       ----------
+        modelID : int / str
             ID of the Model.
        """       
-       if modelID == " ":
-            raise FieldIsRequired("Hey. This field is required, please input the Model ID!")
        url2 = self.url
        response = self.session.post(f"{url2}/model/market/i-{modelID}/purchase/")
        stscd = response.status_code
@@ -386,19 +360,17 @@ class KoGaMa:
                                      
     def PurchaseAvatar(self, avatarID):
        """
-       Purchases a Avatar from Shop.
+       - Purchases a Avatar from Shop.\n
        
-       Returns True, If avatar has been bought.
+       Returns True, If avatar has been bought.\n
        Returns False, If fails to buy avatar.
        
-       **Parameters**
-        ----------
-        avatarID : int
-            ID of the Avatar.
+       Parameters:
+       ----------
+          avatarID : int / str
+             ID of the Avatar.
        """
        url2 = self.url
-       if avatarID == " ":
-            raise FieldIsRequired("Hey. This field is required, please input the Avatar ID!")
        response = self.session.post(f"{url2}/model/market/a-{avatarID}/purchase/")
        stscd = response.status_code
        if stscd == 201:
@@ -406,17 +378,18 @@ class KoGaMa:
        elif stscd != 201:
          return False    
     
-    # Elite Category                                
+    # Elite Category
+                                     
     def ClaimEliteGold(self):
        """
-       Claims daily Elite Gold.
+        - Claims daily Elite Gold.\n 
        
-       Returns True, If the gold has been collected.
+       Returns True, If the gold has been collected.\n
        Returns False, If fails to collect gold.
        
-       **Parameters**
-        ----------
-        None
+       Parameters:
+       ----------
+         None..
        """
        url2 = self.url
        uid = self.user_id
@@ -426,8 +399,8 @@ class KoGaMa:
         return True
        elif stscd != 200:
         return False
-    
     # Badges Category
+    
     def ReedemCoupon(self, coupon):
       """
       Reedems a coupon code.
@@ -435,13 +408,11 @@ class KoGaMa:
       Returns True, If the coupon has been reedemed.
       Returns False, If fails to reedem coupon.
        
-      **Parameters**
+      Parameters:
       ----------
       coupon : str
           Coupon Code.
       """
-      if coupon == " ":
-        raise FieldIsRequired("Hey. This field is required, please input the Coupon Code!")
       data = {"code": coupon}
       url2 = self.url
       response = self.session.post(f"{url2}/api/coupon/redeem/", json=data)
@@ -458,17 +429,15 @@ class KoGaMa:
        Returns True, If the badge has been unlocked.
        Returns False, If fails to unlock badge.
        
-       **Parameters**
-        ----------
+       Parameters:
+       ----------
         badge : str
             Badge Name.
         
-        **Notes**
-        ----------
+       Notes:
+       ----------
         This feature is still in development and will likely be finished in future versions..
        """
-       if badge == " ":
-           raise FieldIsRequired("Hey. This field is required, please input the Badge Name!")
        bdg = badge.lower()
        bdg2 = bdg.replace(" ", "_")
        url2 = self.url
@@ -484,30 +453,4 @@ class KoGaMa:
        if stscd == 201:
         return True
        elif stscd != 201:
-        return False
-    
-    # Account Settings
-    
-    def AddEmail(self, email):
-      """
-      Adds email to account.
-       
-      Returns True, If the email has been changed.
-      Returns False, If fails to change email.
-       
-      **Parameters**
-      ----------
-      email : str
-          Account Email.
-      """
-      if email == " ":
-          raise FieldIsRequired("Hey. This field is required, please input your Email!")
-      data = {"email": email}
-      url2 = self.url
-      uid = self.user_id
-      response = self.session.post(f"{url2}/user/{uid}/email/", json=data)
-      stscd = response.status_code
-      if stscd == 200:
-        return True
-      elif stscd != 200:
         return False
