4ec7052ea71b0ffdfa3e47ef2972e373ef9fa76d
TheNoobPro44 <67636254+TheNoobPro44@users.noreply.github.com>
2021-06-14 17:09:00 -0300

    Update __init__.py

--- Kogama/__init__.py
+++ Kogama/__init__.py
@@ -1,1 +1,1 @@
-from .Exceptions import DisallowedURlInput, NotAValidServer, InvalidInformation, FailedLogin, FeedError, TooMuchRequests, ReasonNotFound, TemplateNotFound, FieldIsRequired
+from .Exceptions import DisallowedURlInput, NotAValidServer, FailedLogin, TooMuchRequests, ReasonNotFound, TemplateNotFound
