f8baa1fa581b96c7c1d82126352167b93143bb36
TheNoobPro44 <67636254+TheNoobPro44@users.noreply.github.com>
2021-08-06 17:06:05 -0300

    Update example.py

--- example/example.py
+++ example/example.py
@@ -1,6 +1,6 @@
 from Kogama.kogama import KoGaMa
 
-k = KoGaMa("www") # Selecting our server.. (WWW or BR)
-k.Login("MyCoolUsername", "ReallySecuredPassword") # Loging into our account.
+k = KoGaMa("WWW") # Selecting our server.. (WWW or BR)
+k.Login("MyCoolUsername", "Really_Secured_Password") # Loging into our account.
 k.PostFeed("12345678", "Hii! How are you! ^^") # We input the ID of the user we want to post the feed message on, then our message.
-k.run() # Keeping the player alive / online..
+k.run() # Keeping the player Alive/Online..
